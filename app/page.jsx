fda9c970561bb08f420682825d35239a0a4336bf
Satheesh Bollineni <92820805+satheeshBollineni1998@users.noreply.github.com>
2025-06-06 23:14:53 -0400

    updated link

--- app/page.jsx
+++ app/page.jsx
@@ -27,7 +27,7 @@ const Home = () => {
                   className="uppercase flex items-center gap-2"
                   aria-label="Download CV">
                   <a
-                    href="https://drive.google.com/file/d/10NlnxVltDW6aouUtKP4rzJH3nFInQWIl/view"
+                    href="https://drive.google.com/file/d/1xkqbSVZVY_nchYKQrSHZprz0GRxYozc9/view"
                     download
                     className="flex items-center gap-2 w-full h-full">
                     <span>Download CV</span>
